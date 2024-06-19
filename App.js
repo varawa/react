74a1597bf2a3a787e1c98b6dcac6a53f9d644a7c
varawa <142249631+varawa@users.noreply.github.com>
2024-06-19 12:49:42 +0530

    jsx

--- App.js
+++ App.js
@@ -22,28 +22,41 @@ import ReactDOM from "react-dom/client" ;
 
 const heading1 = React.createElement("h1" , 
     {  
-        id:"title"
+        id:"title",
+        key:"h1"
     }
     , "Heading1"
 ) ;
 const heading2 = React.createElement("h2" , 
     {  
         //Props(they can be anything , not only class/id or style) .
-        id:"title2"
+        id:"title2",
+        key:"h2"
     }
     , "Heading2"
 ) ;
 
+
+////Always have to give keys to the children .
+//Why do we need a key?
+
+//DIFFING ALGORITHM(docs)
+
+
 const container = React.createElement("div", {
-    id: "container"
+    id: "container",
+    key:"c1"
 }
-, [heading1 , heading2]
+, [heading1 , heading2]     
 //Multiple children are passed through array .
 ) 
 
 //Passing a react element inside the root element .
 //console.log(heading) ;
 //A react element is an javascript object .
+
+
+//React.createElement => object => DOM .
 
 
 //React overwrites everything that already exists in root .
@@ -63,3 +76,16 @@ root.render(container) ;
 //React application and its associated components into one or more files to include in a web browser. 
 //This bundling process is critical for enhancing the performance and reducing loading times of React applications.
 
+
+
+//Using createElement is complex and causes errors when building a big structure .
+
+//JSX??
+
+const heading = (<h1 id="title" key="h3">
+        Namaste React
+    </h1>
+) ;
+
+//Is JSX == HTML inside JavaScript ? NO
+//HTML and JSX are look-alikes but not the same thing .
