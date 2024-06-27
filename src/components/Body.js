24aa34c3a67561aec5209e609c2ab0bf1547c8dc
varawa <142249631+varawa@users.noreply.github.com>
2024-06-27 13:29:01 +0530

    config files

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,9 @@
-import { IMG_CDN_URL } from "../../config"
-
 import { restaurantList } from "../../config"
   
+import { RestaurantCard } from "./RestaurantCard"
 //Optional Chaining ?.
 
-const RestaurantCard = ({name , cuisines , cloudinaryImageId ,deliveryTime}) => { 
-    return(
-        <div className="card">
-            <img src={IMG_CDN_URL + cloudinaryImageId} />
-            <h1>{name}</h1>
-            <h2>{cuisines.join(', ')}</h2>
-            <h3>{deliveryTime + "min"}</h3>
-        </div>
-    )
-}
+
 
 //props = properties .
 //props are passed as arguments in the functional component on their own .
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,14 @@
+import { IMG_CDN_URL } from "../../config"
+
+import { restaurantList } from "../../config"
+
+export const RestaurantCard = ({name , cuisines , cloudinaryImageId ,deliveryTime}) => { 
+    return(
+        <div className="card">
+            <img src={IMG_CDN_URL + cloudinaryImageId} />
+            <h1>{name}</h1>
+            <h2>{cuisines.join(', ')}</h2>
+            <h3>{deliveryTime + "min"}</h3>
+        </div>
+    )
+}
